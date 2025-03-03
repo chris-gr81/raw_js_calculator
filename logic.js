c7471a7248d8aaeb6a674f703208736ecb350f60
Christian Grimm <grimmchristian@gmx.de>
2025-03-03 21:32:12 +0100

    Refactor the code to make it easier to read

--- logic.js
+++ logic.js
@@ -1,151 +1,164 @@
 // Globals
 const validNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 const validOperators = ["+", "-", "*", "/"];
-const lowerDisplayTarget = document.getElementById("lower-display");
-const upperDisplayTarget = document.getElementById("upper-display");
+const lowerDisplay = document.getElementById("lower-display");
+const upperDisplay = document.getElementById("upper-display");
 const historyList = document.getElementById("history-list");
-let inputSequence = [];
-let leftOperant = null;
-let rightOperant = null;
-let operator = null;
-let equalCache = false;
+const calculator = {
+  inputSequence: [],
+  leftOperant: null,
+  rightOperant: null,
+  operator: null,
+  equalCache: false,
+};
 
-function validateInput(userInput) {
-  if (equalCache) {
-    // If coming from equals
-    if (validOperators.includes(userInput)) {
-      // if operator follows on equals
-      operator = userInput;
-      drawUpperDisplay(leftOperant + " " + operator);
-      drawLowerDisplay(leftOperant);
-      equalCache = false;
-    } else if (validNumbers.includes(userInput)) {
-      // if number follows on equals
-      inputSequence.push(Number(userInput));
-      drawLowerDisplay(arrayToNumber());
-      drawUpperDisplay("clear");
-      equalCache = false;
-      leftOperant = null;
-      rightOperant = null;
-      operator = null;
-    }
-  } else if (!equalCache) {
-    if (validNumbers.includes(userInput)) {
-      // Input is a number
-      inputSequence.push(Number(userInput));
-      drawLowerDisplay(arrayToNumber());
-    } else if (validOperators.includes(userInput)) {
-      // Input is an operator
-      if (inputSequence.length === 0) {
-        operator = userInput;
-        if (upperDisplayTarget.innerHTML !== "") {
-          upperDisplayTarget.innerHTML =
-            upperDisplayTarget.innerHTML.slice(0, -1) + operator;
-        }
-      } else {
-        if (leftOperant === null) {
-          leftOperant = arrayToNumber();
-          operator = userInput;
-          inputSequence = [];
-          drawUpperDisplay(
-            upperDisplayTarget.innerHTML + " " + leftOperant + " " + operator
-          );
-        } else if (leftOperant !== null) {
-          rightOperant = arrayToNumber();
-          leftOperant = calcExpression();
-          operator = userInput;
-          inputSequence = [];
-          drawUpperDisplay(
-            upperDisplayTarget.innerHTML + " " + rightOperant + " " + operator
-          );
-          drawLowerDisplay(leftOperant);
-          rightOperant = null;
-        }
-      }
-      //
-    } else if (userInput === "=") {
-      // Input is equal-sign
-      rightOperant = arrayToNumber();
-      inputSequence = [];
-      leftOperant = calcExpression();
-      drawLowerDisplay(leftOperant);
-      drawUpperDisplay(
-        upperDisplayTarget.innerHTML + " " + rightOperant + " ="
-      );
-      drawHistory();
-      equalCache = true;
-    }
+function controlLogic(userInput) {
+  if (validNumbers.includes(userInput)) {
+    // is number
+    processNumber(userInput);
+  } else if (validOperators.includes(userInput)) {
+    // is operator
+    processOperator(userInput);
+  } else if (userInput === "=") {
+    // is equal-button
+    processEquals(userInput);
+  } else if (userInput === "CE") {
+    // is CE-button
+    processClear(userInput);
   }
-  if (userInput === "CE") {
-    // Input is CE
-    drawHistory("clear");
-    drawLowerDisplay("clear");
-    drawUpperDisplay("clear");
-    inputSequence = [];
-    leftOperant = null;
-    rightOperant = null;
-    operator = null;
-    equalCache = false;
+}
+
+function processNumber(userInput) {
+  calculator.inputSequence.push(Number(userInput));
+  renderLowerDisplay(arrayToNumber());
+
+  if (calculator.equalCache) {
+    renderUpperDisplay("clear");
+    calculator.leftOperant = null;
+    calculator.rightOperant = null;
+    calculator.operator = null;
+    calculator.equalCache = false;
   }
+}
+
+function processOperator(userInput) {
+  if (calculator.equalCache) {
+    calculator.operator = userInput;
+    renderUpperDisplay(calculator.leftOperant + " " + calculator.operator);
+    renderLowerDisplay(calculator.leftOperant);
+    calculator.equalCache = false;
+    return;
+  }
+
+  if (calculator.inputSequence.length === 0 && upperDisplay.innerHTML !== "") {
+    // wenn keine zahl im cache (vorherige Eingabe war auch Operator)
+    calculator.operator = userInput;
+    upperDisplay.innerHTML =
+      upperDisplay.innerHTML.slice(0, -1) + calculator.operator;
+  } else {
+    // wenn zahl im cache
+    if (calculator.leftOperant === null) {
+      calculator.leftOperant = arrayToNumber();
+      calculator.operator = userInput;
+    } else {
+      calculator.rightOperant = arrayToNumber();
+      calculator.leftOperant = calcExpression();
+      calculator.operator = userInput;
+    }
+    calculator.inputSequence = [];
+    renderUpperDisplay(
+      `${upperDisplay.innerHTML} ${
+        calculator.rightOperant ?? calculator.leftOperant
+      } ${calculator.operator}`
+    );
+    renderLowerDisplay(calculator.leftOperant);
+    calculator.rightOperant = null;
+  }
+}
+
+function processEquals(userInput) {
+  calculator.rightOperant = arrayToNumber();
+  calculator.inputSequence = [];
+  calculator.leftOperant = calcExpression();
+
+  renderLowerDisplay(calculator.leftOperant);
+  renderUpperDisplay(
+    upperDisplay.innerHTML + " " + calculator.rightOperant + " ="
+  );
+
+  calculator.equalCache = true;
+}
+
+function processClear(userInput) {
+  renderHistory("clear");
+  renderLowerDisplay("clear");
+  renderUpperDisplay("clear");
+  calculator.inputSequence = [];
+  calculator.leftOperant = null;
+  calculator.rightOperant = null;
+  calculator.operator = null;
+  calculator.equalCache = false;
 }
 
 function arrayToNumber() {
   let sequenceNumber = 0;
-  for (let i = 0; i < inputSequence.length; i++) {
+  for (let i = 0; i < calculator.inputSequence.length; i++) {
     sequenceNumber +=
-      inputSequence[i] * Math.pow(10, inputSequence.length - 1 - i);
+      calculator.inputSequence[i] *
+      Math.pow(10, calculator.inputSequence.length - 1 - i);
   }
   return sequenceNumber;
 }
 
 function calcExpression() {
   let result = 0;
-  if (operator === "+") {
-    result = leftOperant + rightOperant;
-  } else if (operator === "-") {
-    result = leftOperant - rightOperant;
-  } else if (operator === "*") {
-    result = leftOperant * rightOperant;
-  } else if (operator === "/") {
-    result = leftOperant / rightOperant;
+  if (calculator.operator === "+") {
+    result = calculator.leftOperant + calculator.rightOperant;
+  } else if (calculator.operator === "-") {
+    result = calculator.leftOperant - calculator.rightOperant;
+  } else if (calculator.operator === "*") {
+    result = calculator.leftOperant * calculator.rightOperant;
+  } else if (calculator.operator === "/") {
+    result = calculator.leftOperant / calculator.rightOperant;
   }
+  renderHistory(result);
   return result;
 }
 
-function drawLowerDisplay(option) {
+function renderLowerDisplay(option) {
   if (option === "clear") {
-    lowerDisplayTarget.innerHTML = "0";
+    lowerDisplay.innerHTML = "0";
   } else {
-    lowerDisplayTarget.innerHTML = option;
+    lowerDisplay.innerHTML = option;
   }
 }
-function drawUpperDisplay(option) {
+function renderUpperDisplay(option) {
   if (option === "clear") {
-    upperDisplayTarget.innerHTML = "";
+    upperDisplay.innerHTML = "";
   } else {
-    upperDisplayTarget.innerHTML = option;
+    upperDisplay.innerHTML = option;
   }
 }
 
-function drawHistory(option) {
+function renderHistory(option) {
   if (option === "clear") {
     historyList.innerHTML = "";
   } else {
     const historyResult = document.createElement("li");
     historyResult.classList.add("history-result");
-    historyResult.textContent = lowerDisplayTarget.innerHTML;
+    historyResult.textContent = option;
     historyList.prepend(historyResult);
     const historyCalc = document.createElement("li");
     historyCalc.classList.add("history-calc");
-    historyCalc.textContent = upperDisplayTarget.innerHTML;
+    historyCalc.textContent = `${calculator.leftOperant} ${calculator.operator} ${calculator.rightOperant} =`;
     historyList.prepend(historyCalc);
   }
 }
 
-drawLowerDisplay("clear");
+renderLowerDisplay("clear");
 
 // Eventlistener to intercept button-inputs
 const btnField = document.querySelector(".btn-field");
 btnField.addEventListener("click", (event) => {
-  validateInput(event.target.textContent);
+  controlLogic(event.target.value);
 });
